a56dcd933c1dc3ddc4b070b1ef43426c1f64c560
manue <manue@MSI>
2023-04-23 19:23:04 -0500

    1234

--- Utilidades/signalCropper.m
+++ Utilidades/signalCropper.m
@@ -1,12 +1,34 @@
-function signal = signalCropper(signal)
-    sampleSize = 2^round(log2(length(signal)));
-    if length(signal) ~= sampleSize
-        offset = length(signal) - sampleSize;
-        if mod(offset,2) == 0
-            signal = signal(offset/2+1:end-offset/2);
+function signal = signalCropper(signal,n)
+    sampleSize = [1024 512 256 128 64 32 16 8 4 2 1];
+    if length(signal) ~= sampleSize(n+1)
+        offset = length(signal) - sampleSize(n+1);
+        if offset==1
+            signal=signal(2:end);
+        elseif offset>1
+            if mod(offset,2) == 0
+                signal = signal(offset/2+1:end-offset/2);            
+            else
+                index = floor(offset/2);
+                signal = signal(index+1:end-index-1);
+            end
         else
-            index = floor(offset/2);
-            signal = signal(index+1:end-index-1);
+             if mod(offset,2) == 0
+                offset=abs(offset);
+                if  iscolumn(signal)
+                    signal = [zeros(offset/2,1); signal; zeros(offset/2,1)];    
+                else
+                    signal = [zeros(1,offset/2) signal zeros(1,offset/2)];  
+                    p=1;
+                end
+             else
+                offset=abs(offset);
+                index = floor(offset/2);
+                if  iscolumn(signal)
+                    signal = [zeros(index+1,1); signal; zeros(index-1,1)];
+                else
+                    signal = [zeros(1,index+1) signal zeros(1,index-1)];
+                end
+            end
         end
     end
 end
