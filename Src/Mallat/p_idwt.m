f9447c8a1d2e33d551165f5d4edde7bffbc610c6
manue <manue@MSI>
2023-04-23 19:24:14 -0500

    1234

--- Src/Mallat/p_idwt.m
+++ Src/Mallat/p_idwt.m
@@ -2,7 +2,7 @@
 %Universidad del Cauca 
 %María Manuela Silva Zambrano
 
-function recx = p_idwt(s, w, hs, gs)
+function recx = p_idwt(s, w, hs, gs, n)
     %p_idwt es una función personalizada para aplicar la IDWT sobre un vector
     %hs son los coeficientes para representar la función scaling 
     %gs son los coeficientes para representar la función wavlet
@@ -12,13 +12,28 @@ function recx = p_idwt(s, w, hs, gs)
     
     %dado que la IDWT es un algoritmo iterativo recursivo, esta función
     %deberá llamarse por cada nivel de descomposición implementado
-        
-    fix = length(w) - length(s); %en caso de que existan diferencias en las longitudes de los coeficientes
-    if fix < 0
-        w = [w; zeros(1, fix)];
-    elseif fix > 0
-        s = [s; zeros(1, fix)];
+     
+    %en este código se tiene que la longitud de las tramas es de 1024
+    %muestras 
+
+    for i=n:-1:1
+        fix = length(w{i}) - length(s); %en caso de que existan diferencias en las longitudes de los coeficientes
+        if fix < 0
+            w{i} = [w{i} zeros(1, abs(fix))];
+        elseif fix > 0
+            s = [s zeros(1, fix)];
+        end
+        s = conv(gs, upsample(w{i}, 2)) + conv(hs, upsample(s, 2));
     end
-    recx = conv(gs, upsample(w, 2)) + conv(hs, upsample(s, 2));
-    recx = signalCropper(recx);
+    recx=s;
+    %recx = signalCropper(recx);
+     if length(recx)==1025
+        recx=recx(2:end);
+     else
+        L=length(recx)-1024;
+        if L>0
+            recx=recx(floor(0.5*L)+1:floor(0.5*L)+1024); %se recordan las 2L-1 muestras que agrega el proceso de convolución
+        end
+     end
+    p=1;
 end
