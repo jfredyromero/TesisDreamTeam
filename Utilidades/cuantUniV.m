5bbdbca746bff20fd821bdf75432eec86eda3de8
Jhon Fredy Romero <jfredyrom@gmail.com>
2023-05-05 17:49:24 -0500

    Cuantificador actualizado.

--- Utilidades/cuantUniV.m
+++ Utilidades/cuantUniV.m
@@ -3,36 +3,15 @@
 %María Manuela Silva Zambrano
 %Función para la cuantificación uniforme de una señal
 
-function yc=cuantUniV(y,n)
+function yc = cuantUniV(y, n)
 %y es la señal de entrada a cuantificar 
-%n es el número de niveles de cuantificación
+%n es el número de niveles de cuantificación y debe ser una potencia entera
+%de 2 
 %yc es la señal cuantificada 
-valores=uniquetol(y); %valores de la señal a cuantificar
-
-if length(valores)>n
-    %y=y/max(abs(y));
-    ma=max(y); %máximo valor de la señal de audio
-    mi=min(y); %mínimo valor de la señal de audio 
-    rango=ma-mi; %rango dinámico de la señal 
-    paso=rango/n;
-    p2=paso/2;
-    Am=zeros(1,n);
-    Am(1)=mi+p2;
-    for i=2:n
-        Am(i)=Am(i-1)+paso;
-    end
-    Te=zeros(1,n-1);%valores umbrales para realizar la comparación
-    Te(1)=mi+paso;
-     for i=2:n-1
-        Te(i)=Te(i-1)+paso;
-     end
-    yc=zeros(length(y),1); 
-    yc(y<=Te(1))=Am(1); 
-    yc(y>=Te(n-1))=Am(n);
-    for i=2: n-1
-        yc(y>Te(i-1) & y<=Te(i))=Am(i);
-    end
-else
-    yc=y;
-end
+    [senalNormalized, C, S] = normalize(y, "range");
+    senalMoved = 2 * senalNormalized - 1; % señal normalizada entre -1 y 1
+    delta = 2/n;
+    senalQuantified = delta * round(senalMoved / delta) - sign(senalMoved) * delta / 2;
+    senalQuantifiedNormalized = (senalQuantified + 1) / 2; % señal cuantificada y normalizada entre 0 y 1
+    yc = senalQuantifiedNormalized * S + C;
 end
