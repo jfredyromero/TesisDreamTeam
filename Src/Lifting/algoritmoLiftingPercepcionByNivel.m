689f9cb43b7a6a0cc4a92a3c23c05ee8712a0d60
Jhon Fredy Romero <jfredyrom@gmail.com>
2023-05-24 19:14:35 -0500

    Correccion en caracterización.

--- Src/Lifting/algoritmoLiftingPercepcionByNivel.m
+++ Src/Lifting/algoritmoLiftingPercepcionByNivel.m
@@ -23,11 +23,11 @@ fw = "db1";
 %--------------------------FILTROS LIFTING---------------------------------
 lsc = liftingScheme('Wavelet', fw);
 %--------------------NÚMERO DE NIVELES DE DESCOMPOSICIÓN-------------------
-n = 1;
+n = 10;
 %--------------------NÚMERO DE NIVELES DE CUANTIFICACIÓN-------------------
 q = 16;
 %--------------------CAMA INICIAL DE BITS POR MUESTRA----------------------
-cama = 4;
+cama = 3;
 
 
 %% Lectura de la señal de voz
@@ -92,9 +92,9 @@ bitsMaximosPerTrama = bitsPerSample * tramaSamples;
 %--------------MATRIZ DE BITS ASIGNADOS POR COEFICIENTES-------------------
 coefBits = ones(1, n + 1)' * cama;
 
-aux = totalCoef(:, 1);
+tamanosCoeficientes = cellfun(@(x) x(1), cellfun(@size, totalCoef(:, 1), 'UniformOutput', false));
 for i = 1:n + 1
-    coefBits(i) = length(aux{i}) * coefBits(i);
+    coefBits(i) = tamanosCoeficientes(i) * coefBits(i);
 end
 
 %----------------------PORCENTAJES DE ENERGÍA------------------------------
@@ -106,43 +106,62 @@ bitsAsignadosPerTrama = sum(coefBits);
 
 % Si se asignan más de (1024 * cama) bits hay un error
 if(bitsAsignadosPerTrama ~=  tramaSamples * cama)
-    disp("===============================================")
-    disp("ERROR: Se asignaron como cama más bits de los esperados");
-    disp("===============================================")
-    return;
+    error("ERROR: Se asignaron como cama más bits de los esperados");
 end
 
 %----------CANTIDAD DE BITS RESTANTES PARA CADA TRAMA DEL AUDIO------------
 bitsRestantesPerTrama = bitsMaximosPerTrama - bitsAsignadosPerTrama;
 
 if(bitsAsignadosPerTrama > bitsMaximosPerTrama)
-    disp("===============================================")
-    disp("ERROR: Se asignaron más bits de los disponibles");
-    disp("===============================================")
-    return;
+    error("ERROR: Se asignaron más bits de los disponibles");
 end
 
-% Se asignan los bits en base al aporte de energia de cada coeficiente
+% Se asignan los bits en base al aporte de calidad en la percepcion de cada coeficiente
 coefBits = coefBits + floor(bitsRestantesPerTrama * porcentajesPercepcion);
+coefBits = floor(coefBits ./ tamanosCoeficientes) .* tamanosCoeficientes;
+bitsRestantesPerTrama = bitsMaximosPerTrama - sum(coefBits);
+
+[valoresOrdenados, ubicacionesOrdenadas] = sort(porcentajesPercepcion, 'descend');
+i = 1; 
+while bitsRestantesPerTrama ~= 0
+    % Antes de asignar se verifica si alcanza para el numero de muestras que
+    % tiene el coeficiente, sino no se asigna 
+    if bitsRestantesPerTrama >= tamanosCoeficientes(ubicacionesOrdenadas(i))
+        coefBits(ubicacionesOrdenadas(i)) = coefBits(ubicacionesOrdenadas(i)) + tamanosCoeficientes(ubicacionesOrdenadas(i));
+        bitsRestantesPerTrama = bitsRestantesPerTrama - tamanosCoeficientes(ubicacionesOrdenadas(i));
+        i = i + 1;
+        if i == n + 1
+            i = 1;
+        end
+    % Asignación de ultimos bits 
+    else
+        valoresCercanos = tamanosCoeficientes(tamanosCoeficientes <= bitsRestantesPerTrama);
+        valorMasCercano = max(valoresCercanos);
+        indexValorMasCercano = find(tamanosCoeficientes == valorMasCercano);
+        coefBits(indexValorMasCercano(1)) = coefBits(indexValorMasCercano(1)) + tamanosCoeficientes(indexValorMasCercano(1));
+        bitsRestantesPerTrama = bitsRestantesPerTrama - tamanosCoeficientes(indexValorMasCercano(1));
+    end
+end
+
 
 % Si se han asignado más de la totalidad de bits hay un error
 bitsAsignadosPerTrama = sum(coefBits);
 bitsDesperdiciadosPerTrama = bitsMaximosPerTrama - bitsAsignadosPerTrama;
 if(bitsAsignadosPerTrama > bitsMaximosPerTrama)
-    disp("===============================================")
-    disp("ERROR: Se asignaron más bits de los disponibles");
-    disp("===============================================")
-    return;
+    error("ERROR: Se asignaron más bits de los disponibles");
 end
 
 %-------------------BITS A UTILIZAR POR CADA MUESTRA-----------------------
-bitsPerMuestra = zeros(n + 1, 1);
+bitsPerMuestra = floor(coefBits ./ tamanosCoeficientes);
 %----------------------BITS SOBRANTES POR MUESTRA--------------------------
-bitsDesperdiciadosPerNivel = zeros(n + 1, 1);
-for i = 1:n + 1
-    bitsPerMuestra(i) = floor(coefBits(i) / length(aux{i}));
-    bitsDesperdiciadosPerNivel(i) = mod(coefBits(i), length(aux{i}));
+bitsDesperdiciadosPerNivel = mod(coefBits, tamanosCoeficientes);
+
+% Si la suma de bits desperdiciados por nivel es mayor a cero significa que
+% no se estan asignando la totalidad de bits.
+if sum(bitsDesperdiciadosPerNivel) ~= 0
+    error("ERROR: Se han desperdiciado bits");
 end
+
 %---------------------MATRIZ NIVELES DE CUANTIFICACION---------------------
 qPerNivelDescomp = 2.^(bitsPerMuestra);
 
@@ -156,10 +175,7 @@ bitsUsadosPerTrama = sum(bitsUsadosPerNivel);
 % Si la suma de los bits usados y los bits desperdiciados es diferente de
 % la cantidad maxima de bits a usar hay un error
 if (bitsUsadosPerTrama + bitsDesperdiciadosPerTrama) ~= bitsMaximosPerTrama
-    disp("===============================================")
-    disp("ERROR: Ocurrio un error en la asignación de bits");
-    disp("===============================================")
-    return;
+    error("ERROR: Ocurrio un error en la asignación de bits");
 end
 
 bitsDesperdiciados = bitsDesperdiciadosPerTrama * 100 / bitsMaximosPerTrama;
--- Src/caracterizacionLifting.m
+++ Src/caracterizacionLifting.m
@@ -16,11 +16,9 @@ n = 10;
 
 % Familias Wavelet en común entre Lifting y Mallat 
 fw = {'db1', 'db2', 'db3', 'db4', 'db5', 'db6', 'db7', 'db8', 'sym2', 'sym3', 'sym4', 'sym5', 'sym6', 'sym7', 'sym8', ...
-    'coif2', 'bior1.1', 'bior1.3', 'bior1.5', 'bior2.2', 'bior2.4', 'bior2.6'};
-% fw = {'bior2.8', 'bior3.1', 'bior3.3', ...
-%     'bior3.5', 'bior3.7', 'bior3.9', 'bior4.4', 'bior5.5', 'bior6.8', 'rbio1.1', 'rbio1.3', 'rbio1.5', 'rbio2.2', ...
-%     'rbio2.4', 'rbio2.6', 'rbio2.8', 'rbio3.1', 'rbio3.3', 'rbio3.5', 'rbio3.7', 'rbio3.9', 
-% fw = {'rbio4.4', 'rbio5.5', 'rbio6.8'};
+    'coif2', 'bior1.1', 'bior1.3', 'bior1.5', 'bior2.2', 'bior2.4', 'bior2.6', 'bior2.8', 'bior3.1', 'bior3.3', ...
+    'bior3.5', 'bior3.7', 'bior3.9', 'bior4.4', 'bior5.5', 'bior6.8', 'rbio1.1', 'rbio1.3', 'rbio1.5', 'rbio2.2', ...
+    'rbio2.4', 'rbio2.6', 'rbio2.8', 'rbio3.1', 'rbio3.3', 'rbio3.5', 'rbio3.7', 'rbio3.9', 'rbio4.4', 'rbio5.5', 'rbio6.8'};
 
 % Creación de los filtros para cada familia Wavelet
 lsc = cell(1, length(fw));
@@ -49,7 +47,7 @@ for f = 1:length(lsc) % Wavelets madre
             processedSignal = liftingCaracterizacionByCoefFunction(audioCell{i, 2}, n, z, lsc{f});
             originalSignal = downsample(audioCell{i, 2}, 3);
             originalSignal = originalSignal(1:length(processedSignal))'; % Corto la señal original al tamaño de la procesada y la transpongo
-            pesq = medirPESQ(originalSignal, processedSignal) / 4.5;
+            pesq = (medirPESQ(originalSignal, processedSignal) + 0.5) / 5;
             nmse = medirNMSE(originalSignal, processedSignal);
             audioResults{i, z + 1} = (pesq + nmse) / 2;
         end
