98c5cf5b2e08c69a852a39eeb121c93c6dfdd827
Jhon Fredy Romero <jfredyrom@gmail.com>
2023-06-15 20:17:59 -0500

    CuantUniNew ya funciona para todas las variaciones de parámetros

--- Utilidades/cuantUniVNew.m
+++ Utilidades/cuantUniVNew.m
@@ -8,25 +8,24 @@ function yc = cuantUniVNew(y, n)
 %n es el número de niveles de cuantificación y debe ser una potencia entera
 %de 2 
 %yc es la señal cuantificada 
-    minimo= min(y);
-    maximo= max(y);
-    diferencia= maximo-minimo;
-    escalon = diferencia/n;
-%    % if n == 2
-%         partition = [minimo,maximo];
-%         codebook = minimo+escalon;
-%         
-%     else
-        partition = minimo+escalon:escalon:maximo-escalon;
-        codebook = minimo+0.5*(escalon):escalon:maximo-0.5*(escalon);
+    minimo = min(y);
+    maximo = max(y);
+    diferencia = maximo - minimo;
+    escalon = diferencia / n;
+    if n == 2
+        partition = minimo + escalon;
+        codebook = [minimo, maximo];        
+    else
+        partition = minimo + escalon:escalon:(minimo + (escalon * (n - 1)));
+        codebook = minimo + 0.5 * (escalon):escalon:(minimo + (escalon * (n - 1)) + 0.5 * escalon);
         % Now optimize, using codebook as an initial guess.
-        [partition2,codebook2] = lloyds(y,codebook);
-        [index,quants,distor] = quantiz(y,partition,codebook);
-        [index2,quant2,distor2] = quantiz(y,partition2,codebook2);
-        yc = quants';
-%     end
-
-    
-    
-
+    end
+    if isempty(partition) 
+        partition = 0 ; 
+        codebook = [0, 0];
+    end 
+    [partition2, codebook2] = lloyds(y, codebook);
+    [index, quants, distor] = quantiz(y, partition, codebook);
+    [index2, quant2, distor2] = quantiz(y, partition2, codebook2);
+    yc = quants'; 
 end
