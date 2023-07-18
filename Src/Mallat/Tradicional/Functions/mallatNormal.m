99c3a4cd4d2da05a897ac19eeb22fb44d213b23b
Jhon Fredy Romero <jfredyrom@gmail.com>
2023-07-18 10:35:54 -0500

    Comparacion de calidad de reconstrucción de Lifting vs Mallat.

---
+++ Src/Mallat/Tradicional/Functions/mallatNormal.m
@@ -0,0 +1,76 @@
+% Trabajo de grado de Ingeniería en Electrónica y Telecomunicaciones 
+% Universidad del Cauca 
+% Dream Team: Jhon Fredy Romero y Lina Virginia Muñoz
+% Función para la cuantificación de una señal en el dominio Wavelet con el
+%   algoritmo de Lifting
+function [quantSignal, quality] = mallatNormal(signal, n, td, ha, ga, hs, gs)
+    % signal es la señal de entrada a cuantificar 
+    % n es el número de niveles de descomposición
+    % q es el número de niveles de cuantificación
+    % td es la duración de cada trama en segundos (0.064, 0.032, 0.016, etc)
+    % cama es el número de bits repartidos inicialmente a cada muestra
+    % lsc es el objeto usado por la funcion de la transformada donde se
+    %   especifica la Wavelet madre en uso
+
+    %% Muestreo de la señal a 16KHz
+    
+    %-----------------------FACTOR DE MUESTREO Y PERIODO---------------------------
+    Fs = 48000;
+    %---------------------------FACTOR DE SUBMUESTREO------------------------------
+    fsm = 3; % Ya que así se muestreará a una frecuencia de 16KHz --> 48KHz/3
+    l = fsm * floor(length(signal) / fsm);
+    signal = signal(1:l);
+    %---------------------------SEÑAL SUBMUESTREADA------------------------------
+    xn = downsample(signal, fsm);
+    fs = Fs / fsm;
+
+
+    %% División de la señal en tramas
+
+    %--------------------LONGITUD DE TRAMA EN MUESTRAS----------------------
+    tramaSamples = round(fs * td);
+    %---------------------------NUMERO DE TRAMAS----------------------------
+    numTramas = floor(length(xn) / tramaSamples);
+    %---------------------------MATRIZ DE TRAMAS----------------------------
+    tramas = zeros(numTramas, tramaSamples);
+    for i = 1:numTramas
+        inicio = (i - 1) * tramaSamples + 1;
+        fin = i * tramaSamples;
+        tramas(i, :) = xn(inicio:fin);
+    end
+
+
+    %% Transformada Wavelet con algoritmo Mallat
+
+    % -----------------------COEFICIENTES SCALING------------------------------
+    scalingCoef = cell([1, numTramas]);
+    %------------------------COEFICIENTES WAVELET------------------------------
+    waveletCoef = cell([n, numTramas]);
+    for i = 1:numTramas
+        [tramaScalingCoef, tramaWaveletCoef] = m_dwt(tramas(i, :), ha, ga, n); 
+        % Se guardan los coeficientes Scaling de la trama
+        scalingCoef{i} = tramaScalingCoef;
+        % Se guardan los coeficientes Wavelet de cada uno de los n niveles de descomposición trama
+        for j = 1:n
+            waveletCoef{j, i} = tramaWaveletCoef{j};
+        end
+    end
+    %------------------------COEFICIENTES TOTALES------------------------------
+    totalCoef = [waveletCoef; scalingCoef];
+    
+    
+    %% Reconstrucción de las tramas y de la señal original
+    
+    senalReconst = 1:numel(tramas);
+    for i = 1:numTramas
+        senalReconst(((i - 1) * tramaSamples) + 1:tramaSamples * i) = p_idwt(totalCoef(:, i), hs, gs);
+    end
+
+    pesq = ((medirPESQ(xn(1:length(senalReconst)), senalReconst')) + 0.5) / 5;
+    nmse = medirNMSE(xn(1:length(senalReconst)), senalReconst');
+
+    % Variables a retornar
+    quantSignal = senalReconst;
+    quality = (pesq + nmse) / 2;
+
+end
---
+++ Src/comparacion.m
@@ -0,0 +1,100 @@
+% Importación de funciones
+addpath('../Grabaciones/');
+addpath('../Mediciones/');
+addpath('../Resultados/Comparacion/');
+addpath('../Src/Lifting/Tradicional/Functions/');
+addpath('../Src/Mallat/');
+addpath('../Src/Mallat/Tradicional/Functions/');
+addpath('../Utilidades/');
+
+% Limpieza de variables
+clear;
+close all;
+clc;
+
+% Cargar datos de los audios previamente almacenados
+load("audioCell.mat");
+
+% Tipo de algoritmo
+mallat = "Mallat";
+lifting = "Lifting";
+
+% Establezco el algoritmo por correr
+algoritmo = mallat;
+
+% Establezco el numero de niveles de descomposición
+n = 9;
+
+% Establezco la duración de cada trama en segundos
+td = 0.064;
+
+% Familias Wavelet en común entre Lifting  
+fw = {'db1', 'db2', 'db3', 'db4', 'db5', 'db6', 'db7', 'db8', 'sym2', 'sym3', 'sym4', 'sym5', 'sym6', 'sym7', 'sym8', ...
+    'coif2', 'bior1.1', 'bior1.3', 'bior1.5', 'bior2.2', 'bior2.4', 'bior2.6', 'bior2.8', 'bior3.1', 'bior3.3', ...
+    'bior3.5', 'bior3.7', 'bior3.9', 'bior4.4', 'bior5.5', 'bior6.8', 'rbio1.1', 'rbio1.3', 'rbio1.5', 'rbio2.2', ...
+    'rbio2.4', 'rbio2.6', 'rbio2.8', 'rbio3.1', 'rbio3.3', 'rbio3.5', 'rbio3.7', 'rbio3.9', 'rbio4.4', 'rbio5.5', 'rbio6.8'};
+
+% Creación de los filtros para cada familia Wavelet
+lsc = cell(1, length(fw));
+for i = 1:length(fw)
+    switch algoritmo
+        case mallat
+            [ha, ga, hs, gs] = wfilters(fw{i});
+            lsc{i} = [ha; ga; hs; gs];
+        case lifting
+            lsc{i} = liftingScheme('Wavelet', fw{i});
+        otherwise
+            error("ERROR: No valid option");
+    end
+    
+end
+
+% Matriz de resultados de las pruebas
+audioResults = load(algoritmo + "-Checkpoint.mat").audioResults;
+
+% Análisis y síntesis con diferentes familias wavelet
+for f = 1:length(lsc) % Wavelets madre
+    disp("===============================================");
+    disp("Inicio de pruebas de Wavelet " + fw{f});
+    for i = 1:length(audioCell) % Audios
+        tic;
+        for j = 1:n % Niveles de descomposicion
+            try
+                switch algoritmo
+                    case mallat
+                        [~, quality] = mallatNormal(audioCell{i, 2}, j, td, lsc{f}(1, :), lsc{f}(2, :), lsc{f}(3, :), lsc{f}(4, :));
+                    case lifting
+                        [~, quality] = liftingNormal(audioCell{i, 2}, j, td, lsc{f});
+                    otherwise
+                        error("ERROR: No valid option");
+                end
+                audioResults(j, f) = audioResults(j, f) + quality;
+            catch
+                warning('Problem using function.  Assigning a value of NaN');
+            end
+        end
+        disp("===============================================");
+        disp("Wavelet " + fw{f} + ": Audio #" + i + " finalizado. Time elapsed: " + toc);
+        disp("===============================================");
+    end
+    disp("Final de pruebas de Wavelet " + fw{f} + ".");
+    disp("===============================================")
+    save("../Resultados/Comparacion/" + algoritmo + "-Checkpoint.mat", "audioResults");
+end
+
+columnsNames = cell(1, length(lsc));
+for i = 1:length(lsc)
+    columnsNames{i} = fw{i};
+end
+
+rowsNames = cell(n, 1);
+for i = 1:n
+    rowsNames{i} = "n = " + i;
+end
+
+% Promedia los resultados y cambia los ceros por NaN
+totalResults = audioResults / length(audioCell);
+
+% Guarda resultados en un archivo .mat
+resultados = array2table(totalResults,  'VariableNames', string(columnsNames), 'RowNames', string(rowsNames));
+save("../Resultados/Comparacion/" + algoritmo + ".mat", "resultados");
